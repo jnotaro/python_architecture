3ff23af47cf03a12a65b858588f2ee8ac414fa02
Grzegorz Kocjan <kkocyk@gmail.com>
2019-04-28 20:01:14 +0200

    Add minimalistic unit test to show power of clean arch

--- shop/unittests/test_logic.py
+++ shop/unittests/test_logic.py
@@ -16,3 +16,15 @@ def test_add_product_increase_orders_total_cost(
 
     order = orders.get(1)
     assert order.total_cost == 100
+
+
+def test_add_product_increase_orders_total_cost_minimal(
+    prepare_repositories: StaticRepositories
+) -> None:
+    from bf_shop.logic import OrderLogic
+
+    logic = OrderLogic(*prepare_repositories)
+
+    order = logic.add_product(order_id=1, product_id=1)
+
+    assert order.total_cost == 100
